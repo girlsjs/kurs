c086f38669ce1083e6ef62083467f0d7c590950e
Arim Seventh <arimeq@gmail.com>
2016-09-16 00:20:56 +0200

    Gallery with changing pictures.

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,2 +1,29 @@
-var $gallery = document.querySelector("gallery");
+var $gallery = document.querySelector("#gallery");
 var $picture = $gallery.querySelector("img");
+var $prev = document.querySelector("#prev");
+var $next = document.querySelector("#next");
+var index = 0;
+var maxIndex = 5;
+
+$prev.addEventListener('click', onPrev);
+$next.addEventListener('click', onNext);
+
+function onPrev() {
+    index = index - 1;
+    if (index < 0) {
+        index = maxIndex;
+    }
+    moveGalleryToIndex();
+}
+
+function onNext() {
+    index = index + 1;
+    if (index > maxIndex) {
+        index = 0;
+    }
+    moveGalleryToIndex();
+}
+
+function moveGalleryToIndex() {
+    $gallery.scrollLeft = 400 * index;
+}
