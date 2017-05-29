2cba657c962533e0892cdbdce682bccb3a17aaa7
Mchl <mchlpl@gmail.com>
2017-05-29 23:11:20 +0200

    Krok z pętlami

--- gallery/js/step5.js
+++ gallery/js/step5.js
@@ -3,31 +3,6 @@ let currentNumber = 1;
 document.querySelector( '#slide' + currentNumber ).classList.add( 'show' );	
 document.querySelector( '#pin' + currentNumber ).classList.add( 'selected' );
 
-let playing = false;
-let slideDuration = 3500;
-let slideshowInterval = null;
-
-function playButtonClicked ( event ) {
-	if ( playing === true ) {
-		stopSlideshow();
-	} else {
-		startSlideshow();
-	}
-	//event.stopPropagation();
-}
-document.querySelector( '#play' ).addEventListener( 'click', playButtonClicked );
-
-function startSlideshow( ) {
-	document.querySelector( '#play' ).classList.add('on');	
-	playing = true;
-	slideshowInterval = setInterval( showNextSlide, 3000 );	
-}
-
-function stopSlideshow( ) {
-	document.querySelector( '#play' ).classList.remove( 'on' );	
-	playing = false;
-	clearInterval( slideshowInterval );	
-}
 
 function showSlide ( newNumber ) {
     document.querySelector( '.show' ).classList.remove( 'show' );
@@ -52,7 +27,7 @@ function showNextSlide ( ) {
 	if( newNumber > 5 ) {
 		newNumber = 1;
 	}
-    showSlide( newNumber );
+  showSlide( newNumber );
 }
 document.querySelector('#next').addEventListener( 'click', showNextSlide );
 
@@ -61,13 +36,6 @@ function showPreviousSlide ( ) {
 	if( newNumber < 1 ) {
 		newNumber = 5;
 	}
-    showSlide( newNumber );
+  showSlide( newNumber );
 }
 document.querySelector('#prev').addEventListener( 'click', showPreviousSlide );
-
-function maybeStopSlideshow ( ) {
-	if ( playing === true ) {
-		stopSlideshow();
-	}
-}
-//document.querySelector( '#wrapper' ).addEventListener( 'click', maybeStopSlideshow );
