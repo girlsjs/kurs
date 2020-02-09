5bb2e4f2ad1e6e15e4d95163b6b27cb63e4f6e6f
arimeq <arimeq@gmail.com>
2020-02-09 17:58:08 +0100

    Updated stuff in ZIP (also some whitespace) (#77)

--- follow-ups/functions/notes.js
+++ follow-ups/functions/notes.js
@@ -1,25 +1,25 @@
 
 function getNote() {
-  let element = document.createElement( 'div' );
-  element.classList.add( 'note' );
+  let element = document.createElement('div');
+  element.classList.add('note');
   return element;
 }
 
-function addNoteToBoard( note ) {
-  document.querySelector( '#board' ).appendChild( note );
+function addNoteToBoard(note) {
+  document.querySelector('#board').appendChild(note);
 }
 
-function showNewNoteForm () {
+function showNewNoteForm() {
   document.querySelector("#overlay").classList.add('active');
 }
 
-document.querySelector("#pen").addEventListener( 'click', showNewNoteForm );
+document.querySelector("#pen").addEventListener('click', showNewNoteForm);
 
-function submitNewNoteForm ( ) {
+function submitNewNoteForm() {
   const text = document.querySelector('#new-note-text').value || undefined;
   document.querySelector('#new-note-text').value = "";
-  createNewNote ( text );
+  createNewNote(text);
   document.querySelector("#overlay").classList.remove('active');
 }
 
-document.querySelector("#new-note-add").addEventListener( 'click', submitNewNoteForm );
+document.querySelector("#new-note-add").addEventListener('click', submitNewNoteForm);
